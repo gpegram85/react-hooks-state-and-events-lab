e08bc700f0c8c2a30956d60b2ea7d2a5827920f6
Grey Pegram <dpegram85@gmail.com>
2024-04-24 22:33:21 -0400

    Completed Lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+
+const [cartStatus, setCartStatus] = useState(false)
+const [productStatus, setProductStatus] = useState(false)
+
+function handleCart() {
+  setCartStatus(!cartStatus)
+  setProductStatus(!productStatus)
+}
+
   return (
-    <li className="">
+    <li className={productStatus ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCart}>{cartStatus ? "Remove From Cart" : "Add To Cart"}</button>
     </li>
   );
 }
